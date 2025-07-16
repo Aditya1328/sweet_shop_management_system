d04b0baf6f7fcd545d2d580e472af11d793aab74
Aditya1328 <dhruval20042006@gmail.com>
2025-07-16 14:51:18 +0530

    Added test for deleting sweet

--- test_shop.py
+++ test_shop.py
@@ -6,3 +6,10 @@ def test_add_sweet():
     sweet = Sweet(id=1, name='Ladoo', category='candy', price=10.0, quantity=50)
     shop.add_sweet(sweet)
     assert shop.get_sweet(1).name == 'Ladoo'
+
+def test_delete_sweet():
+    shop = SweetShop()
+    sweet = Sweet(id=2, name='Barfi', category='pastry', price=15.0, quantity=20)
+    shop.add_sweet(sweet)
+    shop.delete_sweet(2)
+    assert shop.get_sweet(2) is None
