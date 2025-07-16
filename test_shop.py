e38b2173d1390aaa0fdc598f8686dabc9392e54e
Aditya1328 <dhruval20042006@gmail.com>
2025-07-16 15:03:13 +0530

    Added test for viewing all sweets

--- test_shop.py
+++ test_shop.py
@@ -13,3 +13,12 @@ def test_delete_sweet():
     shop.add_sweet(sweet)
     shop.delete_sweet(2)
     assert shop.get_sweet(2) is None
+
+def test_view_sweets():
+    shop = SweetShop()
+    shop.add_sweet(Sweet(id=3, name='Rasgulla', category='candy', price=12.0, quantity=30))
+    shop.add_sweet(Sweet(id=4, name='Jalebi', category='candy', price=8.0, quantity=40))
+    sweets = shop.view_sweets()
+    assert len(sweets) == 2
+    assert sweets[0].name == 'Rasgulla'
+    assert sweets[1].name == 'Jalebi'
